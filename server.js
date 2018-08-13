4f35d7d6bc48513479a8f604e2fdc71cc56edc95
Kartik Raja S <kartiksubu.krs@gmail.com>
2018-08-13 21:45:11 +0530

    Just a Setup

---
+++ server.js
@@ -0,0 +1,28 @@
+var express    = require("express");
+ var mysql      = require('mysql');
+// var email   = require("emailjs/email");
+ var connection = mysql.createConnection({
+   host     : 'localhost',
+   user     : 'root',
+   password : 'admin',
+   database : 'transport'
+ });
+var bodyParser = require('body-parser');
+ var app = express();
+
+app.use(express.static('app'));
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/', function (req, res) {
+   res.sendFile("app/index.html" );
+})
+
+
+function setvalue(){
+  console.log("calling setvalue.....");
+}
+var server = app.listen(8083, function () {
+var host = server.address().address
+var port = server.address().port
+console.log("Example app listening at http://%s:%s", host, port)
+});
