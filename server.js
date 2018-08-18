c1ca48cecb798fd4b39f2de9fa00e5f98a251611
Kartik Raja S <kartiksubu.krs@gmail.com>
2018-08-19 00:11:26 +0530

    initial setup of admin panel

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var express    = require("express");
  var connection = mysql.createConnection({
    host     : 'localhost',
    user     : 'root',
-   password : 'admin',
-   database : 'transport'
+   password : '',
+   database : 'db_festive_lights'
  });
+
 var bodyParser = require('body-parser');
  var app = express();
 
@@ -17,6 +18,67 @@ app.get('/', function (req, res) {
    res.sendFile("app/index.html" );
 })
 
+//select the username and password from login table
+app.get('/login-card',  urlencodedParser,function (req, res)
+{
+
+  var username={"user_name":req.query.username};
+    var password={"login_pwd":req.query.password};
+    //console.log(username+'  '+password);
+       connection.query('SELECT * from master_user where ? and ?',[username,password],
+        function(err, rows)
+        {
+    if(!err)
+    {
+    if(rows.length>0)
+    {
+        console.log(rows);
+      res.status(200).json({'returnval': rows });
+    }
+    else {
+      {
+          res.status(200).json({'returnval': 0 });
+      }
+    }
+  }
+    else
+    {
+      console.log(err);
+      res.status(200).json({'returnval': 'invalid'});
+    }
+
+});
+  });
+
+
+/*get newly generated ID sequence*/
+app.post('/getIDsequence',  urlencodedParser,function (req, res)
+{
+  console.log("getIDsequence");
+       connection.query('SELECT * from master_id_generate',
+        function(err, rows)
+        {
+    if(!err)
+    {
+    if(rows.length>0)
+    {
+        console.log(rows);
+      res.status(200).json({'returnval': rows });
+    }
+    else {
+      {
+          res.status(200).json({'returnval': 0 });
+      }
+    }
+  }
+    else
+    {
+      console.log(err);
+      res.status(200).json({'returnval': 'invalid'});
+    }
+
+});
+  });
 
 function setvalue(){
   console.log("calling setvalue.....");
